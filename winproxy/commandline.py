a56d36d90a4e92981ea6785a10644d7aba437798
André Alexander Bell <github@andre-bell.de>
2016-01-26 22:38:40 +0100

    Some more changes to switch from argparse to click

--- winproxy/commandline.py
+++ winproxy/commandline.py
@@ -7,8 +7,86 @@
 
 # In[ ]:
 
-import argparse
+#import argparse
+import click
 from . import ProxySetting
+
+
+# In[ ]:
+
+def _to_bool(value):
+    try:
+        i = int(value)
+        return i == 1
+    except:
+        if value in ['True', 'true', 'yes']:
+            return True
+        if value in ['False', 'false', 'no']:
+            return False
+        raise Exception('Can\'t convert {0} to a boolean value'.format(value))
+
+
+# In[ ]:
+
+# Requires getting parameters from subcommands....
+# Well... To be done...
+@click.group()
+def winproxy():
+    """The command line function"""
+    pass
+    
+    ##############################################################
+    ## argparse structure
+    ##############################################################
+    ## Create a parser
+    #parser = argparse.ArgumentParser(prog='winproxy')
+    #
+    ## The command will accept subparsers
+    #cmd_parsers = parser.add_subparsers(dest='command', help='Get help on command')
+    #
+    ## The following commands are available through their subparsers
+    #parser_add = cmd_parsers.add_parser('add', help=cmd_add.__doc__)
+    #parser_add.set_defaults(func=cmd_add)
+    #
+    #parser_cpl = cmd_parsers.add_parser('cpl', help=cmd_cpl.__doc__)
+    #parser_cpl.set_defaults(func=cmd_cpl)
+    #
+    #parser_del = cmd_parsers.add_parser('del', help=cmd_del.__doc__)
+    #parser_del.set_defaults(func=cmd_del)
+    #
+    #parser_edit = cmd_parsers.add_parser('edit', help=cmd_edit.__doc__)
+    #parser_edit.set_defaults(func=cmd_edit)
+    #
+    #parser_off = cmd_parsers.add_parser('off', help=cmd_off.__doc__)
+    #parser_off.set_defaults(func=cmd_off)
+    #
+    #parser_on = cmd_parsers.add_parser('on', help=cmd_on.__doc__)
+    #parser_on.set_defaults(func=cmd_on)
+    #
+    #parser_set = cmd_parsers.add_parser('set', help=cmd_set.__doc__)
+    #parser_set.add_argument('--enable', '-e', type=_to_bool)
+    #parser_set.add_argument('--http11', type=_to_bool)
+    #parser_set.add_argument('--override', '-o', default=[], nargs='*')
+    #parser_set.add_argument('--all', dest='proxy', default='')
+    #parser_set.add_argument('--http', dest='http', default='')
+    #parser_set.add_argument('--https', dest='https', default='')
+    #parser_set.add_argument('--ftp', dest='ftp', default='')
+    #parser_set.add_argument('--socks', dest='socks', default='')
+    #parser_set.set_defaults(func=cmd_set)
+    #
+    #parser_view = cmd_parsers.add_parser('view', help=cmd_view.__doc__)
+    #parser_view.add_argument('--max-overrides', '-n', type=int, default=None, help='Limit the number of displayed proxy overrides')
+    #parser_view.set_defaults(func=cmd_view)
+    #
+    #args = parser.parse_args()
+    #try:
+    #    # Python 3 does not display usage if command is ommitted
+    #    # Hence, we force it...
+    #    if not args.command:
+    #        raise AttributeError
+    #    args.func(args)
+    #except AttributeError:
+    #    parser.print_usage()
 
 
 # In[ ]:
@@ -42,6 +120,7 @@ def cmd_edit(args):
 
 # In[ ]:
 
+@winproxy.command(name='off')
 def cmd_off(args):
     """Disable the proxy"""
     p = ProxySetting()
@@ -52,6 +131,7 @@ def cmd_off(args):
 
 # In[ ]:
 
+@winproxy.command(name='on')
 def cmd_on(args):
     """Enable the proxy"""
     p = ProxySetting()
@@ -62,7 +142,16 @@ def cmd_on(args):
 
 # In[ ]:
 
-def cmd_set(args):
+@winproxy.command(name='set')
+@click.option('--enable', '-e', 'enable') #type=_to_bool)
+@click.option('--http11', 'http11') #type=_to_bool)
+@click.option('--override', '-o', 'override') #default=[], nargs='*')
+@click.option('--all', 'proxy') #, default='')
+@click.option('--http', 'http') #, default='')
+@click.option('--https', 'https') #, default='')
+@click.option('--ftp', 'ftp') #, default='')
+@click.option('--socks', 'socks') #, default='')
+def cmd_set(enable, http11, override, proxy, http, https, ftp, socks):
     """Change the current proxy settings"""
     enable = args.enable
     http11 = args.http11
@@ -101,78 +190,11 @@ def cmd_set(args):
 
 # In[ ]:
 
-def cmd_view(args):
+@winproxy.command(name='view')
+@click.option('--max-overrides', '-n', 'max_overrides', help='Limit the number of displayed proxy overrides') #type=int, default=None, 
+def cmd_view(max_overrides):
     """The view command displays the current proxy settings"""
     p = ProxySetting()
     p.registry_read()
-    p.display(args.max_overrides)
-
-
-# In[ ]:
-
-def _to_bool(value):
-    try:
-        i = int(value)
-        return i == 1
-    except:
-        if value in ['True', 'true', 'yes']:
-            return True
-        if value in ['False', 'false', 'no']:
-            return False
-        raise Exception('Can\'t convert {0} to a boolean value'.format(value))
-
-
-# In[ ]:
-
-def winproxy():
-    """The command line function"""
-    # Create a parser
-    parser = argparse.ArgumentParser(prog='winproxy')
-    
-    # The command will accept subparsers
-    cmd_parsers = parser.add_subparsers(dest='command', help='Get help on command')
-
-    # The following commands are available through their subparsers
-    parser_add = cmd_parsers.add_parser('add', help=cmd_add.__doc__)
-    parser_add.set_defaults(func=cmd_add)
-    
-    parser_cpl = cmd_parsers.add_parser('cpl', help=cmd_cpl.__doc__)
-    parser_cpl.set_defaults(func=cmd_cpl)
-    
-    parser_del = cmd_parsers.add_parser('del', help=cmd_del.__doc__)
-    parser_del.set_defaults(func=cmd_del)
-    
-    parser_edit = cmd_parsers.add_parser('edit', help=cmd_edit.__doc__)
-    parser_edit.set_defaults(func=cmd_edit)
-    
-    parser_off = cmd_parsers.add_parser('off', help=cmd_off.__doc__)
-    parser_off.set_defaults(func=cmd_off)
-    
-    parser_on = cmd_parsers.add_parser('on', help=cmd_on.__doc__)
-    parser_on.set_defaults(func=cmd_on)
-    
-    parser_set = cmd_parsers.add_parser('set', help=cmd_set.__doc__)
-    parser_set.add_argument('--enable', '-e', type=_to_bool)
-    parser_set.add_argument('--http11', type=_to_bool)
-    parser_set.add_argument('--override', '-o', default=[], nargs='*')
-    parser_set.add_argument('--all', dest='proxy', default='')
-    parser_set.add_argument('--http', dest='http', default='')
-    parser_set.add_argument('--https', dest='https', default='')
-    parser_set.add_argument('--ftp', dest='ftp', default='')
-    parser_set.add_argument('--socks', dest='socks', default='')
-    parser_set.set_defaults(func=cmd_set)
-
-    parser_view = cmd_parsers.add_parser('view', help=cmd_view.__doc__)
-    parser_view.add_argument('--max-overrides', '-n', type=int, default=None, help='Limit the number of displayed proxy overrides')
-    parser_view.set_defaults(func=cmd_view)
-    
-    args = parser.parse_args()
-    try:
-        # Python 3 does not display usage if command is ommitted
-        # Hence, we force it...
-        if not args.command:
-            raise AttributeError
-        args.func(args)
-    except AttributeError:
-        parser.print_usage()
+    p.display(max_overrides)
 
