3462d7cf31aef293856456496e9293087879ac12
André Alexander Bell <github@andre-bell.de>
2016-01-29 19:26:53 +0100

    Some clean up and disabling database commands

--- winproxy/commandline.py
+++ winproxy/commandline.py
@@ -13,75 +13,17 @@ from . import ProxySetting
 
 # In[ ]:
 
-# Requires getting parameters from subcommands....
-# Well... To be done...
 @click.group()
 @click.version_option(prog_name='winproxy')
 def winproxy():
     """The command line function"""
     pass
-    
-    ##############################################################
-    ## argparse structure
-    ##############################################################
-    ## Create a parser
-    #parser = argparse.ArgumentParser(prog='winproxy')
-    #
-    ## The command will accept subparsers
-    #cmd_parsers = parser.add_subparsers(dest='command', help='Get help on command')
-    #
-    ## The following commands are available through their subparsers
-    #parser_add = cmd_parsers.add_parser('add', help=cmd_add.__doc__)
-    #parser_add.set_defaults(func=cmd_add)
-    #
-    #parser_cpl = cmd_parsers.add_parser('cpl', help=cmd_cpl.__doc__)
-    #parser_cpl.set_defaults(func=cmd_cpl)
-    #
-    #parser_del = cmd_parsers.add_parser('del', help=cmd_del.__doc__)
-    #parser_del.set_defaults(func=cmd_del)
-    #
-    #parser_edit = cmd_parsers.add_parser('edit', help=cmd_edit.__doc__)
-    #parser_edit.set_defaults(func=cmd_edit)
-    #
-    #parser_off = cmd_parsers.add_parser('off', help=cmd_off.__doc__)
-    #parser_off.set_defaults(func=cmd_off)
-    #
-    #parser_on = cmd_parsers.add_parser('on', help=cmd_on.__doc__)
-    #parser_on.set_defaults(func=cmd_on)
-    #
-    #parser_set = cmd_parsers.add_parser('set', help=cmd_set.__doc__)
-    #parser_set.add_argument('--enable', '-e', type=_to_bool)
-    #parser_set.add_argument('--http11', type=_to_bool)
-    #parser_set.add_argument('--override', '-o', default=[], nargs='*')
-    #parser_set.add_argument('--all', dest='proxy', default='')
-    #parser_set.add_argument('--http', dest='http', default='')
-    #parser_set.add_argument('--https', dest='https', default='')
-    #parser_set.add_argument('--ftp', dest='ftp', default='')
-    #parser_set.add_argument('--socks', dest='socks', default='')
-    #parser_set.set_defaults(func=cmd_set)
-    #
-    #parser_view = cmd_parsers.add_parser('view', help=cmd_view.__doc__)
-    #parser_view.add_argument('--max-overrides', '-n', type=int, default=None, help='Limit the number of displayed proxy overrides')
-    #parser_view.set_defaults(func=cmd_view)
-    #
-    #args = parser.parse_args()
-    #try:
-    #    # Python 3 does not display usage if command is ommitted
-    #    # Hence, we force it...
-    #    if not args.command:
-    #        raise AttributeError
-    #    args.func(args)
-    #except AttributeError:
-    #    parser.print_usage()
 
 
-# In[ ]:
-
-@winproxy.command(name='add')
-def _add():
-    """The add command adds the current proxy settings to the database"""
-    click.echo('Adding to database is not yet implemented')
-
+# @winproxy.command(name='add')
+# def _add():
+#     """The add command adds the current proxy settings to the database"""
+#     click.echo('Adding to database is not yet implemented')
 
 # In[ ]:
 
@@ -93,21 +35,15 @@ def _cpl():
     subprocess.Popen(['control', 'inetcpl.cpl'])
 
 
-# In[ ]:
+# @winproxy.command(name='del')
+# def _del():
+#     """Remove a particular proxy setting from the database"""
+#     click.echo('Removing from database is not yet implemented')
 
-@winproxy.command(name='del')
-def _del():
-    """Remove a particular proxy setting from the database"""
-    click.echo('Removing from database is not yet implemented')
-
-
-# In[ ]:
-
-@winproxy.command(name='edit')
-def _edit():
-    """Open the Windows proxy settings dialog"""
-    click.echo('Opening the windows proxy settings dialog is not yet implemented')
-
+# @winproxy.command(name='edit')
+# def _edit():
+#     """Open the Windows proxy settings dialog"""
+#     click.echo('Opening the windows proxy settings dialog is not yet implemented')
 
 # In[ ]:
 
