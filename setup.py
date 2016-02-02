00671016ebaccc86901899bcc42e269a0202e717
André Alexander Bell <github@andre-bell.de>
2016-02-02 19:38:32 +0100

    Added click dependency to setup.py

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['six'],
+    install_requires=['six', 'click>=6'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
